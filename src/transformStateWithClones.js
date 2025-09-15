102bc227eec7ee0e8888b2df10013daa490c957e
Anastasiia Levochkina <nastya.mixaylova98@gmail.com>
2025-09-16 00:51:31 +0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,34 +1,45 @@
 'use strict';
+
 function transformStateWithClones(state, actions) {
-  let prev = { ...state };
+  let prevState = { ...state };
   const history = [];
 
   for (const action of actions) {
-    let next;
+    let nextState = { ...prevState };
 
-    if (action.type === 'clear') {
-      next = {};
-    } else if (action.type === 'addProperties') {
-      const extra =
-        action.extraData &&
-        typeof action.extraData === 'object'
-          ? action.extraData
-          : {};
-      next = { ...prev, ...extra };
-    } else if (action.type === 'removeProperties') {
-      const keys = Array.isArray(action.keysToRemove)
-        ? action.keysToRemove
-        : [];
-      next = { ...prev };
-      for (const key of keys) {
-        delete next[key];
+    switch (action.type) {
+      case 'clear': {
+        nextState = {};
+        break;
       }
-    } else {
-      next = { ...prev };
+
+      case 'addProperties': {
+        const extra =
+          action.extraData && typeof action.extraData === 'object'
+            ? action.extraData
+            : {};
+        nextState = { ...prevState, ...extra };
+        break;
+      }
+
+      case 'removeProperties': {
+        const keys = Array.isArray(action.keysToRemove)
+          ? action.keysToRemove
+          : [];
+        nextState = { ...prevState };
+        for (const key of keys) {
+          delete nextState[key];
+        }
+        break;
+      }
+
+      default: {
+        throw new Error(`Unknown action type: ${action.type}`);
+      }
     }
 
-    history.push(next);
-    prev = next;
+    history.push(nextState);
+    prevState = nextState;
   }
 
   return history;
